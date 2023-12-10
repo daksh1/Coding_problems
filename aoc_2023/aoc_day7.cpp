0fb8dd33b1e172e89d121c4e75a8572dc056642c
daksh1 <dkshkndpal@gmail.com>
2023-12-10 11:15:51 -0600

    added day 7 part 2

--- aoc_2023/aoc_day7.cpp
+++ aoc_2023/aoc_day7.cpp
@@ -18,21 +18,130 @@ void sort_hands_of_same_type(vector<char> order,vector<string> &hand_type_to_han
     }
 }
 
+// int main(){
+//     string line;
+//     ifstream myfile ("input_aoc7");
+//     string hand;
+//     unordered_map<string,int> hand_to_bid,hand_to_rank;
+//     unordered_map<string,vector<string>> hand_type_to_hands;
+//     vector<char> order= {'A','K','Q','J','T','9','8','7','6','5','4','3','2','1'};
+//     int bid,rank=1,total_winnings=0;
+//     if (myfile.is_open()){
+//         while (getline(myfile,line)){
+//             hand=line.substr(0,5);
+//             string sorted_hand=hand;
+//             sort(sorted_hand.begin(),sorted_hand.end());
+//             bid=stoi(line.substr(6,line.length()-6)); 
+//             hand_to_bid[hand]=bid;
+//             if(sorted_hand[0]==sorted_hand[4]){
+//                 hand_type_to_hands["Five of a kind"].push_back(hand);
+//                 continue;
+//             }
+            
+//             int identical_cards,pairs=0,three_alike=0;
+//             for(int i=0;i<sorted_hand.length();i++){
+//                 identical_cards=1;
+//                 while(sorted_hand[i]==sorted_hand[i+1]){
+//                     identical_cards++;
+//                     i++;
+//                 }
+//                 if(identical_cards==4){
+//                     hand_type_to_hands["Four of a kind"].push_back(hand);
+//                     break;
+//                 }
+//                 else if (identical_cards==3){
+//                     three_alike++;
+//                 }
+//                 else if(identical_cards==2){
+//                     pairs++;
+//                 }
+//             }
+//             if(identical_cards==4)
+//                 continue; 
+//             if(three_alike==1 && pairs==1){
+//                 hand_type_to_hands["Full house"].push_back(hand);
+//             }
+//             else if(three_alike ==1){
+//                 hand_type_to_hands["Three of a kind"].push_back(hand);   
+//             }
+//             else if(pairs==2){
+//                 hand_type_to_hands["Two pair"].push_back(hand);
+//             }
+//             else if(pairs==1){
+//                 hand_type_to_hands["One pair"].push_back(hand);
+//             }
+//             else hand_type_to_hands["High card"].push_back(hand);
+//             // cout<< hand<< "   "<< bid<<endl;
+
+//         }
+//         myfile.close();
+//     }
+
+//     else{
+//         cout<<"unable to open file";
+//     }
+//     for(auto it: hand_type_to_hands){
+//         sort_hands_of_same_type(order,it.second);
+//         hand_type_to_hands[it.first]=it.second;
+//     }
+
+//     for(auto jt: hand_type_to_hands["High card"])
+//         hand_to_rank[jt]=rank++;
+//     for(auto jt: hand_type_to_hands["One pair"])
+//         hand_to_rank[jt]=rank++;
+//     for(auto jt: hand_type_to_hands["Two pair"])
+//         hand_to_rank[jt]=rank++;
+//     for(auto jt: hand_type_to_hands["Three of a kind"])
+//         hand_to_rank[jt]=rank++;
+//     for(auto jt: hand_type_to_hands["Full house"])
+//         hand_to_rank[jt]=rank++;
+//     for(auto jt: hand_type_to_hands["Four of a kind"])
+//         hand_to_rank[jt]=rank++;
+//     for(auto jt: hand_type_to_hands["Five of a kind"])
+//         hand_to_rank[jt]=rank++;
+
+//     // for(auto it: hand_type_to_hands){
+//     //     cout<< it.first<<"  ";
+//     //     for(auto jt:it.second){
+//     //         cout<< jt<< "  ";
+//     //     }
+//     //     cout<<endl;
+//     // }
+    
+//     for(auto it: hand_type_to_hands){
+//         for(auto jt:it.second){
+//             total_winnings+=hand_to_bid[jt]*hand_to_rank[jt];
+//         }
+//     }
+//     cout<< total_winnings;
+//     return 0;
+// }
+//part 2
+int find_num_of_jokers_in_hand(string hand){
+    int num_of_jokers_in_hand=0;
+    for(auto it:hand){
+        if(it=='J')
+            num_of_jokers_in_hand++;
+    }
+    return num_of_jokers_in_hand;
+}
+
 int main(){
     string line;
     ifstream myfile ("input_aoc7");
     string hand;
-    unordered_map<string,int> hand_to_bid;
-    vector<int> bids;
+    unordered_map<string,int> hand_to_bid,hand_to_rank;
     unordered_map<string,vector<string>> hand_type_to_hands;
-    int bid; 
+    vector<char> order= {'A','K','Q','T','9','8','7','6','5','4','3','2','1','J'};
+    int bid,num_of_jokers_in_hand,rank=1,total_winnings=0;
     if (myfile.is_open()){
         while (getline(myfile,line)){
             hand=line.substr(0,5);
-            string sorted_hand=hand;
-            sort(sorted_hand.begin(),sorted_hand.end());
             bid=stoi(line.substr(6,line.length()-6)); 
             hand_to_bid[hand]=bid;
+            num_of_jokers_in_hand=find_num_of_jokers_in_hand(hand);
+            string sorted_hand=hand;
+            sort(sorted_hand.begin(),sorted_hand.end());
             if(sorted_hand[0]==sorted_hand[4]){
                 hand_type_to_hands["Five of a kind"].push_back(hand);
                 continue;
@@ -46,7 +155,11 @@ int main(){
                     i++;
                 }
                 if(identical_cards==4){
-                    hand_type_to_hands["Four of a kind"].push_back(hand);
+                    if(num_of_jokers_in_hand==1 || num_of_jokers_in_hand==4){
+                        hand_type_to_hands["Five of a kind"].push_back(hand);
+                        break;
+                    }
+                    hand_type_to_hands["Four of a kind"].push_back(hand); 
                     break;
                 }
                 else if (identical_cards==3){
@@ -59,18 +172,34 @@ int main(){
             if(identical_cards==4)
                 continue; 
             if(three_alike==1 && pairs==1){
-                hand_type_to_hands["Full house"].push_back(hand);
+                if(num_of_jokers_in_hand==3 || num_of_jokers_in_hand==2)
+                    hand_type_to_hands["Five of a kind"].push_back(hand);
+
+                else hand_type_to_hands["Full house"].push_back(hand);
             }
             else if(three_alike ==1){
-                hand_type_to_hands["Three of a kind"].push_back(hand);   
+                if(num_of_jokers_in_hand==3 || num_of_jokers_in_hand==1)
+                    hand_type_to_hands["Four of a kind"].push_back(hand);
+
+                else hand_type_to_hands["Three of a kind"].push_back(hand);   
             }
             else if(pairs==2){
-                hand_type_to_hands["Two pair"].push_back(hand);
+                if(num_of_jokers_in_hand==2)
+                    hand_type_to_hands["Four of a kind"].push_back(hand);
+                else if(num_of_jokers_in_hand==1)
+                    hand_type_to_hands["Full house"].push_back(hand);
+                else hand_type_to_hands["Two pair"].push_back(hand);
             }
             else if(pairs==1){
-                hand_type_to_hands["One pair"].push_back(hand);
+                if(num_of_jokers_in_hand==2 || num_of_jokers_in_hand==1)
+                    hand_type_to_hands["Three of a kind"].push_back(hand);
+                else hand_type_to_hands["One pair"].push_back(hand);
             }
-            else hand_type_to_hands["High card"].push_back(hand);
+            else {
+                if(num_of_jokers_in_hand==1)
+                    hand_type_to_hands["One pair"].push_back(hand);
+                else hand_type_to_hands["High card"].push_back(hand);
+            }
             // cout<< hand<< "   "<< bid<<endl;
 
         }
@@ -80,14 +209,11 @@ int main(){
     else{
         cout<<"unable to open file";
     }
-    unordered_map<string,int> hand_to_rank;
-    vector<char> order= {'A','K','Q','J','T','9','8','7','6','5','4','3','2','1'};
     for(auto it: hand_type_to_hands){
         sort_hands_of_same_type(order,it.second);
         hand_type_to_hands[it.first]=it.second;
     }
 
-    int rank=1;
     for(auto jt: hand_type_to_hands["High card"])
         hand_to_rank[jt]=rank++;
     for(auto jt: hand_type_to_hands["One pair"])
@@ -103,7 +229,6 @@ int main(){
     for(auto jt: hand_type_to_hands["Five of a kind"])
         hand_to_rank[jt]=rank++;
 
-    int total_winnings=0; 
     // for(auto it: hand_type_to_hands){
     //     cout<< it.first<<"  ";
     //     for(auto jt:it.second){
